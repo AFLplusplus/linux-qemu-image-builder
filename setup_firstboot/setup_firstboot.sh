5915967a2b3027831aa759805094ef00f815b60d
Toka <tokazerkje@outlook.com>
2024-11-12 19:12:42 +0100

    oh fuck

--- setup_firstboot/setup_firstboot.sh
+++ setup_firstboot/setup_firstboot.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
 # This script is executed once at first boot
-sleep 20
-# wait till internet is up
-
-yes | pacman -S docker docker-compose git less >>/setup_error 2>&1
-systemctl start docker >>/setup_error 2>&1
-/runtime/setup.sh >>/setup_error 2>&1
-echo "Done" >>/setup_error 2>&1
